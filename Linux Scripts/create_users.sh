e9e79e4de93736e2cd7caf642828f66c75c90829
hudcho <92966081+hudcho@users.noreply.github.com>
2026-01-28 19:41:04 -0500

    Update create_users.sh
    removed line making new users a member of wheel

--- Linux Scripts/create_users.sh
+++ Linux Scripts/create_users.sh
@@ -6,7 +6,7 @@ while IFS= read USER
 do
         echo "Creating user: $USER"
         useradd -m -s /bin/bash "$USER"
-        usermod -aG wheel "$USER"
+
 
         echo "$USER:password1!" | chpasswd
         passwd -e "$USER"
